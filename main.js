9ed23be6f5913ccb41d90bb11306451098707a35
Brooks Patton <brooks.patton@galvanize.com>
2017-08-10 10:37:57 -0600

    lots of growing circles now!

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-let circle;
+const circles = [];
+
 function setup() {
 	createCanvas(1024, 768);
-	const location = createVector(width/2, height/2);
-	circle = new Circle(location);
 }
 
 function draw() {
-	circle.update();
-	circle.display();
+	addCircle();
+
+	circles.forEach(c => {
+		c.update();
+		c.display();
+	});
+}
+
+function addCircle() {
+	const x = random(0, width+1);
+	const y = random(0, height+1);
+
+	const location = createVector(x, y);
+	circles.push(new Circle(location));
 }
